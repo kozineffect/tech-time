3227c112d5ab9a1aefb2bbad8c0b14c5cece33ca
Joe Kozlowski <kozineffect@github.com>
2017-09-13 15:40:51 -0400

    Added a sign up page that allows the user to enter required data, passes it and pushes it into and array and displays it. Pushed to array as a test to see if data was retrieved from forms correctly.

--- tech-time/src/app/sign-up.component.ts
+++ tech-time/src/app/sign-up.component.ts
@@ -5,4 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
-export class SignUpComponent { }
+export class SignUpComponent {
+    values = [];
+    onClick(firstname: string, lastname: string, username: string,
+        email: string, rate: Float32Array, viewers: string, password: string) {
+        this.values.push(firstname);
+        this.values.push(lastname);
+        this.values.push(username);
+        this.values.push(email);
+        this.values.push(rate);
+        this.values.push(viewers);
+        this.values.push(password);
+    }
+}
